8fe40064525dd9cdd431168435842f67272201f0
wangr <wangruiyang1210@gmail.com>
2025-08-31 23:11:03 -0700

    Add files skeleton

---
+++ TicTacToe/src/test/java/edu/cpp/tictactoe/BoardTest.java
@@ -0,0 +1,12 @@
+package edu.cpp.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BoardTest {
+    @Test
+    void smokeTest() {
+        assertTrue(true, "JUnit is working!");
+    }
+}
+
